332c97c28f83ae9783dfd10e889d415feddf0791
mrwilsondj <mrwilsondj@gmail.com>
2014-09-07 23:29:45 -0400

    puzzle js for loop and action code added

--- js/sliding-puzzle.js
+++ js/sliding-puzzle.js
@@ -2,7 +2,7 @@ $(function () { // this is the wrapper function that executes when the html fini
 
     //all our code will be in here...
 
-    var numberOfPieces = 12, // setting variables...
+    var numberOfPieces = 12, // setting variables on top...
     aspect = "3:4",
     aspectW = parseInt(aspect.split(":")[0]),
     aspectH = parseInt(aspect.split(":")[1]),
@@ -24,6 +24,70 @@ $(function () { // this is the wrapper function that executes when the html fini
     previous = {},
     timer,
     currentTime = {},
-    timerDisplay = container.find("#time").find("span");
+    timerDisplay = container.find("#time").find("span"); // set variables end
+
+    for (var x = 0, y = aspectH; x < y; x++) {
+    for (var a = 0, b = aspectW; a < b; a++) {
+        var top = pieceH * x,
+            left = pieceW * a;
+
+        piece.clone()
+             .attr("id", idCounter++)
+             .css({
+                 width: pieceW,
+                 height: pieceH,
+                 position: "absolute",
+                 top: top,
+                 left: left,
+                 backgroundImage: ["url(", path, ")"].join(""),
+                 backgroundPosition: [
+                     "-", pieceW * a, "px ",
+                     "-", pieceH * x, "px"
+                 ].join("")
+        }).appendTo(imgContainer);
+
+        positions.push({ top: top, left: left });
+    }
+} // end for loop
+
+img.remove(); // Removing the original image from the page
+
+container.find("#0").remove(); // Removing the first piece of the puzzle
+
+positions.shift(); // Removing the first item in the positions array
+
+$("#start").on("click", function (e) { // Shuffling the pieces randomly
+    var pieces = imgContainer.children();
+
+    function shuffle(array) {
+        var i = array.length;
+
+        if (i === 0) {
+            return false;
+        }
+        while (--i) {
+            var j = Math.floor(Math.random() * (i + 1)),
+                tempi = array[i],
+                tempj = array[j];
+
+                array[i] = tempj;
+                array[j] = tempi;
+        }
+    }
+
+    shuffle(pieces);
+
+    $.each(pieces, function (i) {
+        pieces.eq(i).css(positions[i]);
+    });
+
+    pieces.appendTo(imgContainer);
+
+    empty.top = 0;
+    empty.left = 0;
+
+    container.find("#ui").find("p").not("#time").remove();
 
 });
+
+}); // function wrapper end
