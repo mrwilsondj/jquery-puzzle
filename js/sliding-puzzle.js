ec85276f42b81512604bd2d6c25ecd4a3e866735
mrwilsondj <mrwilsondj@gmail.com>
2014-09-07 23:17:47 -0400

    adding jquery function wrapper and set some initial variables

---
+++ js/sliding-puzzle.js
@@ -0,0 +1,29 @@
+$(function () { // this is the wrapper function that executes when the html finished loading
+
+    //all our code will be in here...
+
+    var numberOfPieces = 12, // setting variables...
+    aspect = "3:4",
+    aspectW = parseInt(aspect.split(":")[0]),
+    aspectH = parseInt(aspect.split(":")[1]),
+    container = $("#puzzle"),
+    imgContainer = container.find("figure"),
+    img = imgContainer.find("img"),
+    path = img.attr("src"),
+    piece = $("<div/>"),
+    pieceW = Math.floor(img.width() / aspectW),
+    pieceH = Math.floor(img.height() / aspectH),
+    idCounter = 0,
+    positions = [],
+    empty = {
+        top: 0,
+        left: 0,
+        bottom: pieceH,
+        right: pieceW
+    },
+    previous = {},
+    timer,
+    currentTime = {},
+    timerDisplay = container.find("#time").find("span");
+
+});
